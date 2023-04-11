e1c4d6a397be5eb694521301970595f4fc64ce5b
Daniil Posvenchuk <posvenchuk.danik@gmail.com>
2023-04-11 15:33:21 +0300

    first commit

---
+++ Task3.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    +\"\"\"\"\"+\n   (| o o |)\n    |  ^  |\n    | '-' |\n    +-----+");
+            Console.ReadKey();
+        }
+    }
+}
